a1b91e7df30c1332546bb15e4c6b4bb521c69175
unknown <shaikh.ayan737@gmail.com>
2021-07-01 14:31:37 +0530

    Employee present/absent

--- src/com/day3/EmpWageBuilder.java
+++ src/com/day3/EmpWageBuilder.java
@@ -3,7 +3,15 @@ package com.day3;
 public class EmpWageBuilder {
 	public static void main(String args[]) {
 		System.out.println("Welcome to Employee Wage Program");
-				
+		int IS_FULL_TIME=1;
+		double empwage=Math.floor(Math.random() *10)%2;
+		if(empwage==IS_FULL_TIME) {
+			System.out.println("Employe is present");
+		}
+		else {
+			System.out.println("Employe is absent");
+		}
+		
 	}
 
 }
