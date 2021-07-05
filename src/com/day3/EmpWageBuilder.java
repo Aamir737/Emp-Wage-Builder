d08e042067792a6a80d0ae470c00dd18d9e7779a
unknown <shaikh.ayan737@gmail.com>
2021-07-05 23:57:01 +0530

    full time part time

--- src/com/day3/EmpWageBuilder.java
+++ src/com/day3/EmpWageBuilder.java
@@ -5,17 +5,22 @@ public class EmpWageBuilder {
 		System.out.println("Welcome to Employee Wage Program");
 		int WagePerHour=20;
 		int FullDayHour=8;
-		int DailyWage= WagePerHour  * FullDayHour;
-		
+		int IS_PART_TIME=2;
+		int DailyWage= 0;
 		int IS_FULL_TIME=1;
-		double empwage=Math.floor(Math.random() *10)%2;
-		if(empwage==IS_FULL_TIME) {
-			System.out.println("Employe is present and daily wages is:" + DailyWage);
-		}
-		else {
-			System.out.println("Employe is absent");
-		}
+		int EmpHour=0;
 		
-	}
-	
-}
+		double empwage=Math.floor(Math.random() *10)%3;
+		if(empwage==IS_PART_TIME) 
+			EmpHour=8;
+		
+			else if(empwage==IS_FULL_TIME) 
+						EmpHour=4;
+			
+			else 
+			EmpHour=0;
+			DailyWage=WagePerHour*EmpHour;
+			System.out.println("Employe is absent");}}
+
+
+
